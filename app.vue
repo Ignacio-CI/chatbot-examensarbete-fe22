ce89d4255f8cebb77b2c0c7b8ecf1aa7c27e9df9
ignacio-cc <ignacio.cerda@corechange.se>
2024-05-19 10:25:25 +0200

    feat: created form state manager

--- app.vue
+++ app.vue
@@ -9,16 +9,20 @@
       >
         <div class="bg-white border border-slate-200 p-6 rounded">
           <!-- Start Screen -->
-          <Start />
+          <Start v-if="!isChatting"/>
           
 
           <!-- Chat Screen -->
-          <Chat />
+          <Chat v-else/>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+  const isChatting = useState("isChatting", () => false);
+</script>
 
 
 <style>
