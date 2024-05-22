318e544500f1a84305cfce307ee8f16cbb1e322f
ignacio-cc <ignacio.cerda@corechange.se>
2024-05-22 17:54:01 +0200

    feat(core): implmented openai functions

---
+++ server/api/message.post.ts
@@ -0,0 +1,24 @@
+import { client, assistant } from "../utils/openai";
+import { getLatestMessage } from "../utils/get-message";
+
+export default defineEventHandler(async (event) => {
+  const threadID = getCookie(event, "thread-id");
+
+  if (!threadID) {
+    return;
+  }
+
+  const queryParams = getQuery(event);
+
+  await client.beta.threads.messages.create(threadID, {
+    role: "user",
+    content: queryParams.message?.toString() ?? "",
+  });
+
+  const run = await client.beta.threads.runs.create(threadID, {
+    assistant_id: assistant,
+
+  });
+
+  return await getLatestMessage(threadID, run.id);
+});
--- server/api/thread.ts
+++ server/api/thread.ts
@@ -1,10 +1,13 @@
 import { client, assistant } from "../utils/openai";
 
 export default defineEventHandler(async (event) => {
+  const queryParams = getQuery(event);
+
   const thread = await client.beta.threads.create();
 
   const run = await client.beta.threads.runs.create(thread.id, {
     assistant_id: assistant,
+    additional_instructions: `Användarens namn är ${queryParams.name}.`,
   });
 
   return {
