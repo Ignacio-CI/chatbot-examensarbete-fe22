4f0e0a25868994d081e63c3d7d4b07f56773bdc2
ignacio-cc <ignacio.cerda@corechange.se>
2024-05-29 14:30:17 +0200

    changed nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    public: {
+      OPENAI_API_KEY: process.env.OPENAI_API_KEY
+    }
+  }
 })
